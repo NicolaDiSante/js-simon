1bc0e3067e6c03a798fff3b8f1d248d9eb5d59cf
NicolaDiSante <nickymacha90@gmail.com>
2021-04-27 11:00:21 +0200

    Correzione post - lezione

--- script.js
+++ script.js
@@ -1,84 +1,149 @@
-// Al nostro via il computer genera 5 numeri
-// Vengono mostrati per 5 secondi
-// L'utente deve indovinare i 5 numeri
-// C'è un attesa di 5 secondi perchè il computer sta analizzando mostrando "CALCOLO IN CORSO"
-// Vengono mostrati i numeri indovinati
+
+
+
+// CREO VARIABILI GLOBALI DA POTER UTILIZZARE SEMPRE
+// Creo gli array vuoti da poter riempire in seguito
+// creo un limite massimo di numeri da produrre
+// creo una variabile per l'attesa dei secondi
+
+var arrRandom, arrUser, arrResult;
+var limit = 5;
+var secAttesa = 2;
+
 
 $(document).ready(function(){
 
+  // INIZIALIZZO L'APPLICAZIONE
   reset();
 
-  //array vuoto da riempire con i numeri random generati dal programma
-  var arrRandom = [];
-  var arrUserNumbers = [];
-  var arrResult = [];
+  // FACCIO GENERARE L'ARRAY RANDOM CON UN CICLO WHILE
+  // FINO A QUANDO L'ARRAY RANDOM NON RAGGIUNGE LA DIMENSIONE LIMIT DEVO CONTINUARE A BUTTARE DENTRO NUMERI
+  
+ 
+ 
+  //CREO 3 OPZIONI PER TUTTI I CLICK SUI BOTTONI
+  //INIZIO DEL GIOCO
 
-  $('#btn-start').click(function(){
+  $('#start').click(function(){
 
-    $(this).hide();
+    while(arrRandom.length < limit){
+      var nr = getNumberGenerator(1, 100);
+      // Se il numero non è presente lo inserisco nell'array
+      //Se non è vero che arrRandom include nr allora lo mando dentro all'arrayRandom 
+      if(!arrRandom.includes(nr)) arrRandom.push(nr);
+    } 
+
+    //MOSTRO AL DISPLAY I NUMERI RANDOM PER TOT SECONDI
+    printDisplay("I numeri generati sono: " + arrRandom.toString());
+    $('#start').hide();
+
+
+    //TERMINO LA VISUALIZZAZIONE DOPO I SECONDI E INIZIA IL GIOCO
+    // APPARE LA SCRITTA DI INSERIMENTO NUMERI
+    // FACCIO APPARIRE LA MIA CONSOLE
+    setTimeout(function(){
+      printDisplay('Inserisci ' + limit + ' numeri.');
+      $('#console').show();
+    }, secAttesa * 1000);
     
-    while(arrRandom.length < 5){
-      arrRandom.push(numberGenerator(1, 100));
+  });
+
+
+  // INSERISCO I NUMERI
+  $('#send-number').click(function(){
+
+
+    // sul click di send number devo leggere il valore dell'input
+    // Devo inserire suddetto valore dentro all'array user
+    
+    if(arrUser.length < limit){
+
+      if(arrUser.includes($('input').val())) {
+        printDisplay("Attenzione, numero già inserito!");
+      }else{
+        arrUser.push($('input').val());
+        printDisplay("I numeri inseriti sono: " + arrUser.toString());
+      }
+
+    }
+    if(arrUser.length === limit){
+
+       //faccio un ciclo for of su arr user per prelevare num
+       for(var num of arrUser){
+        //usa sempre typeOf() per capire di cosa stai parlando
+        // se num è incluso in arrRandom pushalo dentro ad arrResult
+        if(arrRandom.includes(parseInt(num))){
+          arrResult.push(num)
+        }
+      }
+
+      //Quando ho inserito tutti i numeri aspetto 1 secondo e faccio sparire la console
+      // Per mostrare anche l'ultimo numero inserito aspetto tot secondi a scrivere Risultato in elaborazione
+      // Dopo 2 secondi da Risultato in elabroazione mostro Il risultato è: 
+
+      setTimeout(function(){
+
+        printDisplay("Risultato in elaborazione . . . .");
+        $('#console').hide();
+
+      }, 1000);
+
+      setTimeout(function(){
+
+        
+        if(arrResult.length == 0){
+          printDisplay("Il risultato è: 0");
+        }else{
+          printDisplay("Hai indovinato questi numeri: " + arrResult.toString());
+        }
+        $('#restart').show();
+
+      }, 2000);
+      
     }
     
-    console.log(arrRandom);
-
-    printOutput(arrRandom.toString(), "h1");
-
-    setTimeout(function(){
-
-      printOutput("Indovina i 5 numeri!", "h1");
-      $("#console").show();
-
-      $('#btn-send').click(function(){
-        
-        arrUserNumbers.push($('#input').val());
-        $('#input').val("");
-
-        console.log(arrUserNumbers)
-
-        if(arrUserNumbers.length === 5){
-          
-          $("#console, h1").hide();
-          $('h1').text("Risultato in elaborazione").show();
-
-          setTimeout(function(){
-
-            if(arrUserNumbers.includes(arrRandomNumbers[i])){
-              console.log("Ci sono dei numeri in comune")
-            }
-
-            
-          }, 5000)
-
-        }
-      })
-
-      
-  
-    }, 1000);
-
-    
-    
+    $('input').val(" ");
+    $('input').focus();
 
 
-  })
+  });
 
 
 
-})
+  $('#restart').click(function(){
+    reset();
+  });
 
+
+});
+
+
+//Creo una funzoone che resetti tutto
+// Ci metto dentro gli array vuoti in modo da poter gestire sempre il punto di partenza
+// Di conseguenza azzero tutti i dati e visualizzo la situazione di inizio
 
 function reset(){
-  printOutput("Sei pronto? Clicca sul via", "h1");
-  $('#btn-start').show();
+  limit = parseInt(prompt('scegli quanti numeri inserire'));
+  arrRandom = [];
+  arrUser = [];
+  arrResult = [];
+  printDisplay('Per iniziare a giocare clicca su VIA!');
+  $('#display').show();
+  $('#start').show();
+  $('#restart').hide();
   $('#console').hide();
-};
-
-function printOutput(text, target){
-  $(target).text(text);
 }
 
-function numberGenerator(min, max){
-  return Math.floor(Math.random() * (max - min + 1) + min);
+//Creo una funzione che scriva
+// Stamperà un testo nel display
+
+function printDisplay(text){
+  $('#display').text(text);
+}
+
+
+//Creo generatore di numeri random
+//Accetta i parametri minimo e max
+function getNumberGenerator(min, max){
+  return Math.floor(Math.random() * (max - min) + 1 + min);
 }
